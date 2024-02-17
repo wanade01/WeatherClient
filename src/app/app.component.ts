67d40a38e46dcd4507448c26b68c89012bb3c79f
Waleed <wanade01@gmail.com>
2024-02-16 20:03:33 -0800

    update from 2/16 class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloComponent} from './hello/hello.component'
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HelloComponent, HttpClientModule]
 })
-export class AppComponent {
-  title = 'WeatherClient';
+
+export class AppComponent{
+
+
+
+ 
 }
